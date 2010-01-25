8b67ef51a6bfa2e973baf0e63b19410a40fb5a80
Gilles Dartiguelongue <gilles.dartiguelongue@smartjog.com>
2010-01-25 18:14:34 +0100

    sample: add some documentation

--- share/plugins/sample.py
+++ share/plugins/sample.py
@@ -5,27 +5,35 @@ import time
 PLUGIN_NAME = "sample"
 
 class Job(BaseJob):
+    """ Sample job. """
 
     def __init__(self, log_dir, log_name, log_level, infos):
+        """ Init method of sample job. """
         BaseJob.__init__(self, log_dir, log_name, log_level, infos)
 
     # This method isn't called directly by a check but by another method inside
     # the plugin : its name MUST start with '_'.
     def _do_nothing(self):
+        """ Do nothing. """
         time.sleep(4)
 
     # This method is called by a check.
+    # It is important to document tests with a docstring as this is what will be
+    # used by current scripts to load check's description into the database.
     def nothing(self):
+        """ The nothing check. """
         self.log.info('This check is doing nothing.')
         self._do_nothing()
         self.set_check_status('FINISHED', 'I really did nothing')
 
     # This method is called by a check.
     def pierrot(self):
+        """ The song check. """
         self.log.info('Au clair de la lune.')
         self.set_check_status('FINISHED', 'Nothing to say')
 
 class Plugin(BasePlugin):
+    """ Sample plugin. """
 
     # This dict defines the mandatory options to be configured to use this plugin.
     # Its form is : require = { 'option1_name' : option1_type, ... }
@@ -36,7 +44,9 @@ class Plugin(BasePlugin):
     optional = { }
 
     def __init__(self, log_dir, log_name, log_level, event, url=None, params=None):
+        """ Init method of sample plugin. """
         BasePlugin.__init__(self, PLUGIN_NAME, log_dir, log_name, log_level, event, url, params)
 
     def create_new_job(self, job):
+        """ Create a new sample job. """
         return Job(self.log_dir, self.log_name, self.log_level, job)

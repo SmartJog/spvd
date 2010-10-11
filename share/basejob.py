eac6e06eeeb9f6a94425409d924043eb738d28a6
Alexandre Bossard <alexandre.bossard@smartjog.com>
2010-10-11 17:48:29 +0200

    Fix race condition causing duplicate log lines, closes: #3202

--- share/basejob.py
+++ share/basejob.py
@@ -1,4 +1,5 @@
 """ BaseJob class definitions. """
+from __future__ import with_statement
 
 import logging
 import traceback
@@ -46,30 +47,28 @@ class BaseJob:
 
         # critical section around logger.handlers
         global __handler_lock__
-        __handler_lock__.acquire()
-        if len(logger.handlers) == 0:
-            if options.nodaemon:
-                log_handler = logging.StreamHandler(sys.stdout)
-            else:
-                log_dir = options.logdir + '/' + self.infos['check']['plugin']
-                if os.path.exists(log_dir) is False:
-                    os.mkdir(log_dir)
-                log_file = "%s/%s.log" % (log_dir, self.infos['check']['plugin_check'])
-                log_handler = logging.FileHandler(log_file)
+        with __handler_lock__:
+            if len(logger.handlers) == 0:
+                if options.nodaemon:
+                    log_handler = logging.StreamHandler(sys.stdout)
+                else:
+                    log_dir = options.logdir + '/' + self.infos['check']['plugin']
+                    if os.path.exists(log_dir) is False:
+                        os.mkdir(log_dir)
+                    log_file = "%s/%s.log" % (log_dir, self.infos['check']['plugin_check'])
+                    log_handler = logging.FileHandler(log_file)
 
-            formatter_string = '%(asctime)s %(levelname)-8s %(statusid)5s ' + \
-                               '%(plugin)s:%(check)s %(group)s %(object)s : %(message)s'
-            log_handler.setFormatter(logging.Formatter(formatter_string))
-            logger.addHandler(log_handler)
+                formatter_string = '%(asctime)s %(levelname)-8s %(statusid)5s ' + \
+                        '%(plugin)s:%(check)s %(group)s %(object)s : %(message)s'
+                log_handler.setFormatter(logging.Formatter(formatter_string))
+                logger.addHandler(log_handler)
 
-            if params.get('debug', False):
-                logger.setLevel(logging.DEBUG)
-            else:
-                logger.setLevel(logging.INFO)
+                if params.get('debug', False):
+                    logger.setLevel(logging.DEBUG)
+                else:
+                    logger.setLevel(logging.INFO)
 
-            logger.propagate = False
-        # end of critical section
-        __handler_lock__.release()
+                logger.propagate = False
 
         # Jobs will always use logger_per_job here, even in nodaemon mode,
         # since "spvd.jobs" will trap all log messages in that case.
